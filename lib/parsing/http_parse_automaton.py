6ac75f8a8c4e2391ab48edd0a49d3c68220a38b5
kshakirov <kirill.shakirov4@gmai.com>
2026-07-20 11:04:37 +0300

    wip 3: automaton

--- lib/parsing/http_parse_automaton.py
+++ lib/parsing/http_parse_automaton.py
@@ -15,6 +15,7 @@ class NetworkInput(Enum):
      CHUNK_SIZE_ZERO=2
      DATA_ARRIVED=3
      MALFORMED=4
+     READING_FIXED_DATA=5
 
 
 CONTENT_HEADER= "content-Length"
@@ -37,12 +38,19 @@ def convert_row_http_to_state_input(content_length_headers):
 # as Hal suggested the name is
 def transition(current_state, current_values, current_input):
     match current_state:
-        case State.PARSE_HEADERS if current_input == NetworkInput.CHUNK_SIZE_ZERO:
-            print(f"NOTHING TO READ FROM HTTP BODY QUTTING ...")
+        case State.PARSE_HEADERS:
+            return State.EXPECT_CHUNK_SIZE
+        case State.EXPECT_CHUNK_SIZE if current_input == NetworkInput.CHUNK_SIZE_ZERO:
+            print(f"NOTHING TO READ FROM HTT P BODY QUTTING ...")
             return State.SUCCESS
-        case State.PARSE_HEADERS if current_input== NetworkInput.MALFORMED:
+        case State.EXPECT_CHUNK_SIZE if current_input== NetworkInput.MALFORMED:
             print("ERROR: JUNK")
             return State.ERROR, None
+        case State.EXPECT_CHUNK_SIZE if current_input == NetworkInput.CHUNK_SIZE_GREATER_ZERO:
+            return (State.READ_CHUNK_DATA, current_values, NetworkInput.READING_FIXED_DATA)
+        case State.READ_CHUNK_DATA  if current_values == 0 and  current_input == NetworkInput.READING_FIXED_DATA:
+            return 
+        
 
         case State.PARSE_HEADERS if current_input == NetworkInput.DATA_ARRIVED:
             print(f"TRANSITION TO READING")
