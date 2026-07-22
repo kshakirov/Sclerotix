c56986304d2bd76211d822aa745d9c411d429066
kshakirov <kirill.shakirov4@gmai.com>
2026-07-22 12:27:50 +0300

    automoton wip3

--- lib/parsing/http_parse_automaton.py
+++ lib/parsing/http_parse_automaton.py
@@ -35,46 +35,56 @@ TRANSFER_ENCODING ="transfer-Encoding"
 
 
 def convert_row_http_to_state_input(content_length_headers):
+    print("convert")
+    print(content_length_headers)
+    
     match content_length_headers:
-        case (True, None): TRANSFER_ENCODING, 0
-        case (True, c) if c > 0: TRANSFER_ENCODING, c
-        case (None, 0): CONTENT_HEADER, 0
-        case (None, c)  if c > 0:  CONTENT_HEADER, c
-        case _ : None, None
+        case (True, None):
+            return (TRANSFER_ENCODING, 0)
+        case (True, c) if c > 0:
+            return TRANSFER_ENCODING, c
+        case (None, 0):
+            return CONTENT_HEADER, 0
+        case (None, c)  if c > 0:
+            return CONTENT_HEADER, c
+        case _ :
+            return (NetworkInput.HEADERS_PARSED_EMPTY, None)
 
 
 # as Hal suggested the name is
 def transition(current_state, current_values, current_input):
+    print(current_state, current_values, current_input)
     match current_state:
 
         case State.PARSE_HEADERS if current_input == NetworkInput.HEADERS_PARSED_EMPTY:
-            return State.SUCCESS
+            print("First case")
+            return State.SUCCESS, None, None
         case State.PARSE_HEADERS if current_input == NetworkInput.HEADERS_PARSED_CONTENT_LENGTH:
             return State.READ_CHUNK_DATA, current_values, NetworkInput.READING_FIXED_DATA
         case State.PARSE_HEADERS if current_input == NetworkInput.HEADERS_PARSED_CHUNKED:
             return State.EXPECT_CHUNK_SIZE, current_values, NetworkInput.CHUNK_DATA_FLOW
         case State.EXPECT_CHUNK_SIZE if current_input == NetworkInput.CHUNK_SIZE_ZERO:
-            return State.SUCCESS
+            return State.SUCCESS, None, None
         case State.EXPECT_CHUNK_SIZE if current_input == NetworkInput.MALFORMED:
-            return State.ERROR
+            return State.ERROR,None,None
         case State.EXPECT_CHUNK_SIZE if current_input == NetworkInput.CHUNK_SIZE_GREATER_ZERO:
-            return State.READ_CHUNK_DATA, current_values, current_input == NetworkInput.CHUNK_DATA_FLOW
+            return State.READ_CHUNK_DATA, current_values, NetworkInput.CHUNK_DATA_FLOW
 
         case State.READ_CHUNK_DATA if current_input == NetworkInput.READING_FIXED_DATA and current_values > 0 :
             return State.READ_CHUNK_DATA, current_values, current_input
         case State.READ_CHUNK_DATA if current_input == NetworkInput.READING_FIXED_DATA and current_values ==0 :
-            return State.SUCCESS
+            return State.SUCCESS,None,None
         case State.READ_CHUNK_DATA if current_input == NetworkInput.CHUNK_DATA_FLOW and current_values > 0 :
             return State.READ_CHUNK_DATA, current_values, current_input
         case State.READ_CHUNK_DATA if current_input == NetworkInput.CHUNK_DATA_FLOW and current_values == 0 :
-            return State.EXPECT_CHUNK_SIZE, current_values, current_input == NetworkInput.CHUNK_DATA_FLOW
+            return State.EXPECT_CHUNK_CRLF, current_values, NetworkInput.CHUNK_DATA_EMPTY
 
         case State.READ_CHUNK_DATA if current_input == NetworkInput.CHUNK_DATA_EMPTY :
-            return State.EXPECT_CHUNK_CRLF
+            return State.EXPECT_CHUNK_CRLF, None,None
         case State.EXPECT_CHUNK_CRLF if current_input == NetworkInput.CRLF_VALID:
-            return State.EXPECT_CHUNK_SIZE
+            return State.EXPECT_CHUNK_SIZE,None,None
         case State.EXPECT_CHUNK_CRLF if current_input==NetworkInput.MALFORMED:
-            return State.ERROR
+            return State.ERROR,None,None
         
         
         
@@ -89,19 +99,27 @@ def parse_headers(http_row_data):
 
 def parse_http_content(http_row_data):
     state = State.PARSE_HEADERS
-    headers = parse_headers(http_row_data)
-    content_legnth = (TRANSFER_ENCODING in headers, headers.get(CONTENT_HEADER))
-    netword_intput, current_value = convert_row_http_to_state_input(content_legnth)
-    for i in range(0,1000):
-        newstate, new_value =transition(state, current_value, network_input)
+    headers = parse_headers(http_row_data) # прошлись по заголовкам собрали их
+    #content_legnth = (TRANSFER_ENCODING in headers, headers.get(CONTENT_HEADER))
+    content_legnth = 0
+        # получили кортеж из двух взаимоисключающих загловков
+    #создаем начальный сигнал и начальное значение если оно есть
+    network_input, current_value = convert_row_http_to_state_input(content_legnth)
+    print(network_input, current_value)
+    for i in range(0,3):
+        newstate, new_value, network_input =transition(state, current_value, network_input)
+        print(f"newstate {newstate}, {new_value}")
         match newstate:
             case State.SUCCESS :
                 break #do someting 
             case State.ERROR:
+                #do something
                 break
-            case _ :
-                state = newstate
-                current_value = new_value
-
-    #do someting to finish
+            case State.EXPECT_CHUNK_SIZE:
+                pass
+                # here we listen to socket for receiveng and reading headers
+                # далее пакуему новое состояние с
+            case State.EXPECT_CHUNK_CRLF:
+                pass
     return
+
---
+++ test_auto.py
@@ -0,0 +1,24 @@
+# Чистый GET-запрос без тела
+
+import lib.parsing.http_parse_automaton as p
+import lib.utils.utils as u
+test_http_data = b"GET /index.html HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n"
+post_fixed_data = b"POST /submit HTTP/1.1\r\nHost: localhost\r\nContent-Length: 5\r\n\r\nHello"
+post_chunked_simple = b"POST /stream HTTP/1.1\r\nHost: localhost\r\nTransfer-Encoding: chunked\r\n\r\n5\r\nHello\r\n0\r\n\r\n"
+post_chunked_fragmented = [
+    b"POST /stream HTTP/1.1\r\nHost: localhost\r\nTransfer-Encoding: chunked\r\n\r\n",
+    b"5\r\n",
+    b"Hel",
+    b"lo\r\n",
+    b"0\r\n\r\n"
+]
+
+headers = u.parse_http_req(test_http_data)
+print(headers)
+print("---")
+r = p.convert_row_http_to_state_input(headers)
+
+print(r)
+
+
+p.parse_http_content(headers)
