d60ee2ab6d78ba359632c2feac3323e9ee07b310
kshakirov <kirill.shakirov4@gmai.com>
2026-07-22 12:32:32 +0300

    automoton wip3

--- lib/parsing/http_parse_automaton.py
+++ lib/parsing/http_parse_automaton.py
@@ -101,30 +101,17 @@ def transition(current_state, current_values, current_input):
             print("Failed to find")
             return None,None,None
         
-        
-# это операционный автомат он на вход получает сигнал начальный или вообще он запускается без сигнала
-#потоуму что он запускается только с сигналом чтение заголовков 
-# но пока временно для тестирования у буду передавть сюда состояиния
-def run_engine(state_tuple):
-    #    state,  i_nput = State.PARSE_HEADERS, NetworkInput.HEADERS_PARSED_EMPTY
-    state,  in_put, in_value, data = state_tuple
-    counter = 0
-    while  counter < 24:
-        counter +=1 # времено
-        #for i in range(0,10):
-
-        print(f"run_engine: Entering Loop: state: {state}, in_put: {in_put}, in_value: {in_value}")
-        match state:
-
-def parse_http_content(http_row_data):
+def prep_headers(http_row_data):
     state = State.PARSE_HEADERS
     headers = parse_headers(http_row_data) # прошлись по заголовкам собрали их
     #content_legnth = (TRANSFER_ENCODING in headers, headers.get(CONTENT_HEADER))
     content_legnth = 0
-        # получили кортеж из двух взаимоисключающих загловков
-    #создаем начальный сигнал и начальное значение если оно есть
     network_input, current_value = convert_row_http_to_state_input(content_legnth)
-    print(network_input, current_value)
+    return network_input, current_value)
+
+
+def parse_http_content(state, current_value, network_input):
+    
     for i in range(0,3):
         newstate, new_value, network_input =transition(state, current_value, network_input)
         print(f"newstate {newstate}, {new_value}")
