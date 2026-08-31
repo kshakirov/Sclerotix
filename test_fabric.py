1da44c988d60e5287eaade8aad9376b56c1ab698
kshakirov <kirill.shakirov4@gmai.com>
2026-08-31 10:17:57 +0300

    wip 5

--- test_fabric.py
+++ test_fabric.py
@@ -15,7 +15,7 @@ def make_streaming_request_parser():
       input_buffer = bytearray()
       input_offset = 0
       body_parser_state = p.State.EXPECT_CHUNK_SIZE
-      body_input_offset =0
+      input_offset =0
       body_signal = p.NetworkInput.CHUNK_DATA_EMPTY
       body_current_value=0
       arena = bytearray(32)
@@ -27,18 +27,17 @@ def make_streaming_request_parser():
           nonlocal body_parser_state
           nonlocal body_signal
           nonlocal body_current_value
-          nonlocal input_buffer
-          nonlocal body_input_offset
           nonlocal arena_offset
           nonlocal phase
-          print("I am feed")
+          print("Skipping Phase.Headers for the moment")
+          phase = Phase.BODY
           input_buffer.extend(input_fragment)
-          body_parser_state, body_signal, body_input_offset, body_current_value, arena_offset= p.run_engine(
-        body_parser_state,body_signal, body_current_value, input_buffer, body_input_offset, arena,arena_offset
+          body_parser_state, body_signal, input_offset, body_current_value, arena_offset= p.run_engine(
+        body_parser_state,body_signal, body_current_value, input_buffer, input_offset, arena,arena_offset
     )
           match body_parser_state:
               case p.State.SUCCESS:
-                  return ParserResult.BODY_PARSING_FINISHED, arena
+                  return ParserResult.BODY_PARSING_FINISHED, arena[:arena_offset]
               case _ :
                   return ParserResult.NEED_MORE_DATA, arena
               
