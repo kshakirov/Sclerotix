52d8d2e4b34cf90cc368f14e3936a2e4baa05131
kshakirov <kirill.shakirov4@gmai.com>
2026-08-31 11:08:42 +0300

    wip 7

--- lib/parsing/http_headers_parser.py
+++ lib/parsing/http_headers_parser.py
@@ -12,14 +12,14 @@ class HeaderState(Enum):
     ERROR=8
 
 #пока предполагаем что это все придет одним куском
-def parse_req_header(payload, input_offset, offset_table, state, next_offset_id):
+def parse_req_header(input_fragment, input_offset, offset_table, state, next_offset_id):
     #state =HeaderState.METHOD
     #offset_table = array('i') # на время только
     #next_offset_id =6
     counter = 0
-    while counter < len(payload):
+    while counter < len(input_fragment):
         match state:
-            case HeaderState.METHOD if payload[counter] == 32:
+            case HeaderState.METHOD if input_fragment[counter] == 32:
                  offset_table.insert(0,0)
                  offset_table.insert(1, counter + input_offset)
                  offset_table.insert(2, counter + 1 +input_offset)
@@ -29,7 +29,7 @@ def parse_req_header(payload, input_offset, offset_table, state, next_offset_id)
             case HeaderState.METHOD:
                 counter += 1;
                 # здесь будет защита от некорректного метода или попытки ддос атаки 
-            case HeaderState.REQURI if payload[counter]==32:
+            case HeaderState.REQURI if input_fragment[counter]==32:
                  offset_table.insert(3,counter + input_offset)
                  offset_table.insert(4, counter + 1 + input_offset)
                  counter+=1
@@ -37,31 +37,31 @@ def parse_req_header(payload, input_offset, offset_table, state, next_offset_id)
             case HeaderState.REQURI:
                 counter+=1 
 
-            case HeaderState.REQVERSION if payload[counter]==13:
+            case HeaderState.REQVERSION if input_fragment[counter]==13:
                  offset_table.insert(5,counter + input_offset)
                  counter+=1
                  state=HeaderState.EXPECT_CLRF
             case HeaderState.REQVERSION:
                 counter+=1
-            case HeaderState.EXPECT_CLRF if payload[counter]==10:
+            case HeaderState.EXPECT_CLRF if input_fragment[counter]==10:
                  offset_table.insert(next_offset_id, counter + 1 + input_offset)
                  counter+=1
                  next_offset_id += 1
                  state=HeaderState.HEADER_NAME
-            case HeaderState.HEADER_NAME if payload[counter]==58:
+            case HeaderState.HEADER_NAME if input_fragment[counter]==58:
                  offset_table.insert(next_offset_id, counter + input_offset)
                  next_offset_id += 1
                  offset_table.insert(next_offset_id, counter + 1 + input_offset)
                  next_offset_id += 1
                  state = HeaderState.HEADER_VALUE
                  counter += 1
-            case HeaderState.HEADER_NAME if payload[counter] == 10:
+            case HeaderState.HEADER_NAME if input_fragment[counter] == 10:
                 counter += 1
                 state= HeaderState.SUCCESS
                 #пока не сьедаю все байты только для теста
             case HeaderState.HEADER_NAME:
                 counter += 1
-            case HeaderState.HEADER_VALUE if payload[counter]==13:
+            case HeaderState.HEADER_VALUE if input_fragment[counter]==13:
                 offset_table.insert(next_offset_id, counter + input_offset)
                 next_offset_id += 1
                 state = HeaderState.EXPECT_CLRF
