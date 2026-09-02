9b0b9ede1f23014d9e7acc1e47a8e5db3a84742e
kshakirov <kirill.shakirov4@gmai.com>
2026-09-02 10:10:57 +0300

    wip 18

--- test_http_headers_parser.py
+++ test_http_headers_parser.py
@@ -51,21 +51,19 @@ def cmp_header_names(template, buffer, start, end):
 
 def get_headers(offset_table, payload, template):
       p = payload
-      ot = memoryview(offset_table)[2:]
       ot = offset_table
       base = 6 
       for i in range(floor((len(ot) -  base) /4)):
             r = base + i*4
-                  
             if cmp_header_names(template, p, ot[r], ot[r + 1]):
                   return ot[r + 2], ot[r + 3]
                   
       return None, None
 
-start,end = get_headers(offset_table, raw_get_request, b"Transfer-Encoding")
+start,end = get_headers(offset_table, raw_get_request, b"transfer-encoding")
 print(start,end)
 
-start,end = get_headers(offset_table, raw_get_request, b"transfer-encoDing")
+start,end = get_headers(offset_table, raw_get_request, b"transfer-encoding")
 print(start,end)
 
 
